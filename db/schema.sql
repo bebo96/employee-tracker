75eff61d98f2ad058909cf313fb8e12f71f642bc
bebo <ibrahimimran96@gmail.com>
2022-10-21 16:05:36 -0400

    Clean up code

--- db/schema.sql
+++ db/schema.sql
@@ -24,8 +24,3 @@ CREATE TABLE employee (
   INDEX man_ind (manager_id),
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
-
--- goal is to query the following: 
--- employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
--- SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department ON department.id = role.department_id;
